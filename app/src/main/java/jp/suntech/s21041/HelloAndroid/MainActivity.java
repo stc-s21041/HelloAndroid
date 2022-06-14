e8e68aa61ef315a74f37013abb49ca24121a46e7
admin <ss21041@suntech.jp>
2022-06-14 11:29:46 +0900

    First Commit

---
+++ app/src/main/java/jp/suntech/s21041/HelloAndroid/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.suntech.s21041.HelloAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
